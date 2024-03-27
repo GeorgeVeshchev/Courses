2d1677f7a8bcfac3d34cb747abdceb930d75fd4a
Georgiy Veshchev <ghora2008@gmail.com>
2024-03-27 12:19:19 +0200

    HomeWork#1

---
+++ homework#1/index.js
@@ -0,0 +1,8 @@
+
+alert('Hello!');
+
+const message = prompt('Whats your name?')
+
+const message_two = prompt('Where are you from?');
+
+const result = prompt('Nice to meet you',{message} + 'You are from',{message_two})
